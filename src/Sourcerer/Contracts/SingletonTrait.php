a66bb56a528acd995b9b104a2c546342eb881754
stougeiro <atendimento@sidneytougeiro.com>
2020-10-04 21:02:55 -0300

    First Commit

---
+++ src/Sourcerer/Contracts/SingletonTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+	/**
+	* Sourcerer Contracts
+	* SingletonTrait
+	*
+	* @author        Sidney Tougeiro
+	* @copyright     Copyright(c) 2020, sidneytougeiro.com
+	* @license       MIT
+	*/
+
+    namespace Sourcerer\Contracts;
+
+
+    trait SingletonTrait
+    {
+        private function __construct() {
+        }
+        private function __clone() {
+        }
+        private function __wakeup() {
+        }
+
+        private static $instance;
+
+        public static function getInstance() {
+            if ( ! self::$instance instanceof self) {
+                self::$instance = new self();
+            }
+
+            return self::$instance;
+        }
+    }
+
+?>
